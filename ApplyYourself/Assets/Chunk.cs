e3372fbeceb4b38b7758c82ad75235c6c04ffd7a
Dan-Mechanics <github.tim@desmidt.org>
2026-02-11 15:00:03 +0100

    refactor

--- ApplyYourself/Assets/Chunk.cs
+++ ApplyYourself/Assets/Chunk.cs
@@ -7,61 +7,55 @@ namespace ApplyYourself
         [SerializeField] private MeshFilter filter = default;
         [SerializeField] private MeshCollider coll = default;
         [SerializeField] private MeshColliderCookingOptions options = default;
-        [SerializeField] private int fidelity = default;
-
+        [SerializeField, Min(3)] private int size = default;
         private Mesh mesh;
         private int[] triangles;
-        private float size;
 
-        //  public void Write(ITerrainable terrainable) => MakeNewTerrain(terrainable);
-        //  public void Write(Vector3[] verts) => UpdateMesh(verts);
-
-        public void Setup(float size)
+        private void OnValidate()
         {
-            this.size = size;
-            
+            Setup();
+        }
+
+        public void Setup()
+        {
             mesh = new Mesh();
             filter.mesh = mesh;
             coll.cookingOptions = options;
             mesh.MarkDynamic();
 
-            Vector3[] verts = GenerateVertData(fidelity);
+            Vector3[] verts = GenerateVertData(size);
             SetVerticies(verts);
         }
 
-        private Vector3[] GenerateVertData(int fidelity, float size)
+        private Vector3[] GenerateVertData(int size)
         {
-            Vector3[] verticies = new Vector3[(fidelity + 1) * (fidelity + 1)];
+            Vector3[] verticies = new Vector3[(size + 1) * (size + 1)];
+
             int i = 0;
-            float height = 0f;
-
-            for (int z = 0; z <= fidelity; z++)
+            for (int z = 0; z <= size; z++)
             {
-                for (int x = 0; x <= fidelity; x++)
+                for (int x = 0; x <= size; x++)
                 {
-                    //terrainable.SetHeightStartup(x, ref height, z);
-                    verticies[i] = new Vector3(x, height, z);
-
-                    height = 0f;
+                    verticies[i] = new Vector3(x, Random.value * 10f, z);
                     i++;
                 }
             }
 
             int vert = 0;
             int tris = 0;
-            triangles = new int[fidelity * fidelity * 6];
+            triangles = new int[size * size * 6];
 
-            for (int z = 0; z < fidelity; z++)
+            for (int z = 0; z < size; z++)
             {
-                for (int x = 0; x < fidelity; x++)
+                for (int x = 0; x < size; x++)
                 {
                     triangles[tris + 0] = vert + 0;
-                    triangles[tris + 1] = vert + fidelity + 1;
+                    triangles[tris + 1] = vert + size + 1;
                     triangles[tris + 2] = vert + 1;
                     
                     triangles[tris + 3] = vert + 1;
-                    triangles[tris + 4] = vert + fidelity + 1;
-                    triangles[tris + 5] = vert + fidelity + 2;
+                    triangles[tris + 4] = vert + size + 1;
+                    triangles[tris + 5] = vert + size + 2;
 
                     vert++;
                     tris += 6;
