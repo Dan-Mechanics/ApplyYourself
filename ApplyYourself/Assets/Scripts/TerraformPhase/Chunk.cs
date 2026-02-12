c9b7c631a6df928e82e594aee7e2a4d648e02d4a
Dan-Mechanics <github.tim@desmidt.org>
2026-02-12 19:40:22 +0100

    code chunk

--- ApplyYourself/Assets/Scripts/TerraformPhase/Chunk.cs
+++ ApplyYourself/Assets/Scripts/TerraformPhase/Chunk.cs
@@ -4,62 +4,67 @@ namespace ApplyYourself
 {
     public class Chunk : MonoBehaviour
     {
+        /// <summary>
+        /// not smart idgaf
+        /// </summary>
+        public Vector3[] verticies;
+
         [SerializeField] private MeshFilter filter = default;
         [SerializeField] private MeshCollider coll = default;
         [SerializeField] private MeshColliderCookingOptions options = default;
-        [SerializeField, Min(3)] private int size = default;
-        [SerializeField] private bool showInEditor = default;
         private Mesh mesh;
-        private int[] triangles;
 
-        private void OnValidate()
-        {
-            if (showInEditor)
-                Setup();
+        // add:
+        // center of chunk transofmr
+        // low graphics cube
 
-            showInEditor = false;
-        }
-
-        public void Setup()
+        public void Setup(float[,] heightmap, int width, Vector3 offset, float spacing)
         {
             mesh = new Mesh();
             filter.mesh = mesh;
             coll.cookingOptions = options;
             mesh.MarkDynamic();
 
-            Vector3[] verts = GenerateVertData(size);
-            SetVerticies(verts);
+            verticies = GenerateVertData(heightmap, width, offset, spacing);
+            mesh.triangles = GenerateTriData(width);
+            ReloadMesh();
         }
 
-        private Vector3[] GenerateVertData(int size)
+        private Vector3[] GenerateVertData(float[,] heightmap, int width, Vector3 offset, float spacing)
         {
-            Vector3[] verticies = new Vector3[(size + 1) * (size + 1)];
+            Vector3[] verticies = new Vector3[(width + 1) * (width + 1)];
 
             int i = 0;
-            for (int z = 0; z <= size; z++)
+            for (int z = 0; z <= width; z++)
             {
-                for (int x = 0; x <= size; x++)
+                for (int x = 0; x <= width; x++)
                 {
-                    verticies[i] = new Vector3(x, Random.value * 10f, z);
+                    // also consider other chunks, i want all the verticies to be in worldspace.
+                    verticies[i] = new Vector3(x * spacing, heightmap[x, z], z * spacing) + offset;
                     i++;
                 }
             }
 
+            return verticies;
+        }
+
+        private int[] GenerateTriData(int width)
+        {
             int vert = 0;
             int tris = 0;
-            triangles = new int[size * size * 6];
+            int[] triangles = new int[width * width * 6];
 
-            for (int z = 0; z < size; z++)
+            for (int z = 0; z < width; z++)
             {
-                for (int x = 0; x < size; x++)
+                for (int x = 0; x < width; x++)
                 {
                     triangles[tris + 0] = vert + 0;
-                    triangles[tris + 1] = vert + size + 1;
+                    triangles[tris + 1] = vert + width + 1;
                     triangles[tris + 2] = vert + 1;
-                    
+
                     triangles[tris + 3] = vert + 1;
-                    triangles[tris + 4] = vert + size + 1;
-                    triangles[tris + 5] = vert + size + 2;
+                    triangles[tris + 4] = vert + width + 1;
+                    triangles[tris + 5] = vert + width + 2;
 
                     vert++;
                     tris += 6;
@@ -68,17 +73,19 @@ namespace ApplyYourself
                 vert++;
             }
 
-            return verticies;
+            return triangles;
         }
 
-        private void SetVerticies(Vector3[] verticies)
+        public void ReloadMesh()
         {
             mesh.vertices = verticies;
-            mesh.triangles = triangles;
+            // mesh.triangles = triangles;
             mesh.RecalculateNormals();
 
             Physics.BakeMesh(mesh.GetInstanceID(), false, options);
             coll.sharedMesh = mesh;
+
+            // todo, calculate av and move the big ass cube here.
         }
     }
 }
