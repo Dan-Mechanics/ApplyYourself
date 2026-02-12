ee70869abf9cdd004db1a0b4fb748823f14e8720
Dan-Mechanics <github.tim@desmidt.org>
2026-02-12 21:15:34 +0100

    Revert "working on chunks"
    This reverts commit 0d066b9ebbd6fbee2e4bd0fde1dd1d2f39e938af.

--- ApplyYourself/Assets/Scripts/TerraformPhase/Chunk.cs
+++ ApplyYourself/Assets/Scripts/TerraformPhase/Chunk.cs
@@ -7,51 +7,40 @@ namespace ApplyYourself
         /// <summary>
         /// not smart idgaf
         /// </summary>
-        [HideInInspector] public Vector3[] verticies;
+        public Vector3[] verticies;
 
         [SerializeField] private MeshFilter filter = default;
         [SerializeField] private MeshCollider coll = default;
         [SerializeField] private MeshColliderCookingOptions options = default;
         private Mesh mesh;
 
-        private TerrainManager manager;
-
         // add:
         // center of chunk transofmr
         // low graphics cube
 
-        public void Setup(TerrainManager manager, int vertsAcrossChunk)
+        public void Setup(float[,] heightmap, int width, Vector3 offset, float spacing)
         {
-            this.manager = manager;
-
             mesh = new Mesh();
-            mesh.name = gameObject.name;
             filter.mesh = mesh;
             coll.cookingOptions = options;
             mesh.MarkDynamic();
 
-            // save offset for lods
-
-            verticies = GenerateVerts(vertsAcrossChunk);
-            mesh.triangles = GenerateTris(vertsAcrossChunk);
-
-
-
+            verticies = GenerateVertData(heightmap, width, offset, spacing);
+            mesh.triangles = GenerateTriData(width);
             ReloadMesh();
         }
 
-        private Vector3[] GenerateVerts(int width)
+        private Vector3[] GenerateVertData(float[,] heightmap, int width, Vector3 offset, float spacing)
         {
             Vector3[] verticies = new Vector3[(width + 1) * (width + 1)];
 
             int i = 0;
-            for (int x = 0; x <= width; x++)
+            for (int z = 0; z <= width; z++)
             {
-                for (int z = 0; z <= width; z++)
+                for (int x = 0; x <= width; x++)
                 {
                     // also consider other chunks, i want all the verticies to be in worldspace.
-                    verticies[i] = new Vector3(x, 0f, z);
-                    verticies[i].y = manager.GetHeight(x + transform.position.x, z + transform.position.z);
+                    verticies[i] = new Vector3(x * spacing, heightmap[x, z], z * spacing) + offset;
                     i++;
                 }
             }
@@ -59,12 +48,12 @@ namespace ApplyYourself
             return verticies;
         }
 
-        private int[] GenerateTris(int width)
+        private int[] GenerateTriData(int width)
         {
-            int[] triangles = new int[width * width * 6];
-
             int vert = 0;
             int tris = 0;
+            int[] triangles = new int[width * width * 6];
+
             for (int z = 0; z < width; z++)
             {
                 for (int x = 0; x < width; x++)
@@ -96,7 +85,7 @@ namespace ApplyYourself
             Physics.BakeMesh(mesh.GetInstanceID(), false, options);
             coll.sharedMesh = mesh;
 
-            // todo, calculate avpos for LODS and move the big ass cube here.
+            // todo, calculate av and move the big ass cube here.
         }
     }
 }
