90de2b363c7d37578b669a3f447de0e113edb5a0
Dan-Mechanics <github.tim@desmidt.org>
2026-02-19 22:28:05 +0100

    remove old code

--- ApplyYourself/Assets/Scripts/Composites/Utils.cs
+++ ApplyYourself/Assets/Scripts/Composites/Utils.cs
@@ -1,64 +1,7 @@
-﻿using UnityEngine;
-
-namespace ApplyYourself
+﻿namespace ApplyYourself
 {
     public static class Utils
     {
-        public static int GetIndexByName<T>(T[] array, Ending ending) where T : Object
-        {
-            string str = ending.ToString().ToLowerInvariant();
-            string a = "_" + str;
-            string b = str + "_"; 
-            
-            for (int i = 0; i < array.Length; i++)
-            {
-                string name = array[i].name.ToLowerInvariant();
-                if (name.Contains(a) || name.Contains(b))
-                    return i;
-            }
-
-            Debug.LogWarning($"Could not find element in array --> {str}.");
-            return -1;
-        }
-        
-        public static Vector3 Flatten(Vector3 vector)
-        {
-            vector.y = 0f;
-            return vector;
-        }
-
-        /// <summary>
-        /// Keep scale in mind !!
-        /// </summary>
-        public static bool TryGetIndexFromPos(int x, int z, int maxX, int maxZ, out int index)
-        {
-            index = 0;
-
-            if (x < 0 || x > maxX)
-                return false;
-
-            if (z < 0 || z > maxZ)
-                return false;
-
-            index = x + z * (maxX + 1);
-            return true;
-        }
-
-        public static Vector3Int GetCellPos(Vector3 worldPos, float size)
-        {
-            return new Vector3Int(
-                Mathf.FloorToInt(worldPos.x / size),
-                Mathf.FloorToInt(worldPos.y / size),
-                Mathf.FloorToInt(worldPos.z / size));
-        }
-
-        public static Vector2Int GetCellPos(Vector2 worldPos, float size)
-        {
-            return new Vector2Int(
-                Mathf.FloorToInt(worldPos.x / size),
-                Mathf.FloorToInt(worldPos.y / size));
-        }
-
         public static Ending Filter(Ending ending, EndingOverride[] endingOverrides) 
         {
             for (int i = 0;i < endingOverrides.Length; i++)
--- ApplyYourself/Assets/Scripts/Placeholder/PlaceholderAmbient.cs
+++ ApplyYourself/Assets/Scripts/Placeholder/PlaceholderAmbient.cs
@@ -4,13 +4,9 @@ namespace ApplyYourself
 {
     public class PlaceholderAmbient : BasePlaceholder
     {
-        //private Camera cam;
-        
         private void SetAmbient(Ambient amb)
         {
             Camera cam = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
-            /*if (cam == null)
-                return;*/
 
             RenderSettings.fogDensity = amb.density;
             RenderSettings.fogColor = amb.color;
