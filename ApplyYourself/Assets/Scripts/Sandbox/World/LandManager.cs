332b4518f22b2e926d67bb2e210a88e27388832b
Dan-Mechanics <github.tim@desmidt.org>
2026-04-09 15:37:32 +0200

    small changes

--- ApplyYourself/Assets/Scripts/Sandbox/World/LandManager.cs
+++ ApplyYourself/Assets/Scripts/Sandbox/World/LandManager.cs
@@ -29,11 +29,8 @@ namespace ApplyYourself
             typemap = typemapStartup.GetBulk();
         }
 
-        public void Setup(float[,] waterHeightmap)
-        {
-            this.waterHeightmap = waterHeightmap;
-        }
-        
+        public void Setup(float[,] waterHeightmap) => this.waterHeightmap = waterHeightmap;
+
         private void Update()
         {
             if (removeLand.WasPressed)
@@ -56,11 +53,11 @@ namespace ApplyYourself
             for (int i = positions.Count - 1; i >= 0; i--)
             {
                 Vector2Int pos = positions[i];
-                if (typemap[pos.x, pos.y] == city)
+                /*if (typemap[pos.x, pos.y] == city)
                 {
                     positions.RemoveAt(i);
                     continue;
-                }
+                }*/
 
                 heightmap[pos.x, pos.y] = Mathf.Clamp(heightmap[pos.x, pos.y] + meters, minHeight, maxHeight);
             }
